24d48f64518468deaee71d2338e124d43a62bed4
JouS_1409 <jous_1110@163.com>
2024-02-22 22:22:58 +0800

    写了一个比较基础的运行逻辑 使用dict修改配置文件， 每次修改后检查参数， 运行代码，并对结果进行备份。

---
+++ runMethod.py
@@ -0,0 +1,22 @@
+def modi(cfg,
+         modifier:dict):
+    """
+    修改cfg文件的逻辑，
+    使用setattr 对配置文件进行修改
+    """
+    for key, value in modifier.items():
+        if hasattr(cfg, key):
+            setattr(cfg, key, value)
+        else:
+            raise AttributeError(f"cfg has no attribute {key}")
+
+    # 所有的类使用check 方法对配置参数进行检查
+    # 使用run 方法运行，move 方法备份运行结果
+    cfg.check()
+    cfg.run()
+    cfg.move()
+    return cfg
+
+
+#if __name__ == "__main__":
+
